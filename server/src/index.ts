d67900f045c7c9a0199321e541a82557975b2c67
Edwin Svensson <git@olback.net>
2018-10-23 19:17:16 +0200

    logging, cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import { env } from 'process';
 import * as log from './logger';
 import * as api from './api/api';
@@ -11,6 +12,14 @@ app.listen(env.NODE_PORT, () => {
 
 if (env.NODE_ENV === 'dev' || env.NODE_LOG_REQUESTS === 'true') {
   app.use(log.requests);
+}
+
+if (env.NODE_ENV === 'dev') {
+  app.use(cors());
+} else {
+  app.use(cors({
+    origin: 'http://shopper.ink',
+  }));
 }
 
 app.use('/v1', api.v1);
--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,14 +1,11 @@
 'use strict';
 
-import * as express from 'express';
+import * as morgan from 'morgan';
 import * as c from 'colors';
 
-const router = express.Router();
+// [REQUEST] 172.21.0.1 [Tue, 23 Oct 2018 16:58:12 GMT] GET /path/gfdg 401 Authorization: Bearer eyJhb...wBJEo
 
-router.use(express.json());
-router.use((req, _res, next) => {
-  const now = new Date();
-  const datetime = `[${now.toDateString()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds().toString().length === 1 ? '0' + now.getSeconds().toString() : now.getSeconds().toString()}]`;
+const requests = morgan((tokens, req, res) => {
 
   const method = req.method
     .replace('GET', c.bold(c.blue('GET')))
@@ -17,28 +14,25 @@ router.use((req, _res, next) => {
     .replace('PUT', c.bold(c.yellow('PUT')))
     .replace('DELETE', c.bold(c.red('DELETE')));
 
-  const ip = req.ip.replace('::ffff:', '');
-
-  const line = [ip, datetime, method, req.url];
-
-  for (let p in req.body) {
-    line.push(`${p}=${req.body[p]}`);
-  }
+  const response = [
+    '[REQUEST]'.black.bold,
+    tokens['remote-addr'](req, res).replace('::ffff:', ''),
+    `[${tokens.date(req, res, 'web')}]`,
+    method,
+    tokens.url(req, res),
+    tokens.status(req, res),
+  ];
 
   if (req.headers.authorization) {
-    if (req.headers.authorization.length > 20) { // Bearer: aaa
-      line.push(`Authorization: ${req.headers.authorization.substring(0, 12)}...${req.headers.authorization.substring(req.headers.authorization.length - 5)}`);
+    if (req.headers.authorization.length > 20) {
+      response.push(`Authorization: ${req.headers.authorization.substring(0, 12)}...${req.headers.authorization.substring(req.headers.authorization.length - 5)}`);
     } else {
-      line.push(`Authorization: ${req.headers.authorization}`);
+      response.push(`Authorization: ${req.headers.authorization}`);
     }
   }
 
-  // line.push('--', <string>req.headers['user-agent']);
+  return response.join(' ');
 
-  // console.log(line.join(' '));
-  // log(line.join(' '), `[${req.method}]`);
-  log(line.join(' '), '[REQUEST]');
-  next();
 });
 
 c.setTheme({
@@ -80,4 +74,6 @@ function panic(e: any, label = '') {
   console.log(label.panic || '[PANIC]'.panic, e);
 }
 
-export { router as requests, log, info, ok, warn, error, panic };
+
+
+export { requests, log, info, ok, warn, error, panic };
