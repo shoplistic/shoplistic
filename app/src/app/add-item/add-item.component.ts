e4d2c21cd768100b7e52322b42ce5aa40a93a10e
Edwin Svensson <git@olback.net>
2018-11-07 15:54:30 +0100

    Add item

--- app/src/app/add-item/add-item.component.ts
+++ app/src/app/add-item/add-item.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ShoppingListItem } from '../_classes/shopping-list-item';
+import { ShoppingListService } from '../_services/shopping-list.service';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
   styleUrls: ['./add-item.component.scss']
 })
-export class AddItemComponent implements OnInit {
+export class AddItemComponent implements AfterViewInit {
+
+  @ViewChild('af') af: ElementRef;
 
   item = new ShoppingListItem('', '', '', 1);
 
-  constructor() { }
+  constructor(private _shoppinlistService: ShoppingListService) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.af.nativeElement.focus();
   }
 
   onSubmit() {
     console.log(this.item);
+    this._shoppinlistService.add(this.item).subscribe(
+      res => {
+        console.log('added');
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
 }
