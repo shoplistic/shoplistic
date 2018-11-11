ab638149f9bf609c0d606377f2e79fb84b6d4d8c
Edwin Svensson <git@olback.net>
2018-11-11 01:15:49 +0100

    automated setup

--- README.md
+++ README.md
@@ -1,36 +1,17 @@
 # Not your average shopping list
 
 ### Prerequisites
-* `libpq-dev` is installed.
-* `git` is installed.
-* `docker` is installed.
-* `docker-compose` is installed.
-* [Angular-cli](https://cli.angular.io/) is installed
+* `libpq-dev` (?)
+* `git`
+* `docker`
+* `docker-compose`
+* `node`
+* `npm`
+* [`Angular-cli`](https://cli.angular.io/)
 
 ### Install
 
-1.  Get The Code
-    1. Clone and cd into the directory. `git clone https://github.com/olback/shopper & cd shopper`
-    2. Copy `.env.sample` to `.env`. `cp .env.sample .env`
-    3. Edit `.env` to your liking.
-2.  Install & Build Angular App
-    1. Change directory to the Angular app. `cd app`
-    2. Install dependencies. `npm install`
-    3. Build the app. `ng build --prod`
-    4. Go back to the root of shopper. `cd ..`
-3.  Install & Build Server
-    1. Change directory to the server. `cd server`
-    2. Install dependencies. `npm install`
-    3. Build the server. `npm run ts:compile`
-    4. Go back to the root of shopper. `cd ..`
-4.  Docker Setup
-    1. Start the docker containers. `docker-compose up` (don't add `-d` here)
-5.  Postgres Setup
-    1. Run the setup script in the container. `docker exec shopper_postgres_1 /bin/bash /setup/install.sh`
-6.  Mongo Setup
-    1. Run the setup script in the container. `docker exec shopper_mongo_1 /bin/bash /setup/install.sh`
-7.  Production Ready
-    1. Stop the docker-container command with CTRL+C. (don't force-kill it)
-    2. To automatically run shopper on boot, run `docker-compose up -d`
-8.  Nginx Setup
-    1. Comming later
+1. Get the code: `git clone https://github.com/shopper-ink/shopper`.
+2. Change directory to the code: `cd shopper`.
+3. Run the setup script: `./setup.sh`
+4. Configure nginx. (comming later)
---
+++ setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Get node version
+# NODE_MAJOR_VERSION=$(node -v | cut -b 2,3)
+# if [ $NODE_MAJOR_VERSION != 10 ]; then
+#   echo -e '\e[33mNode version 10 is higly recomended!\e[0m'
+#   sleep 2
+# else
+#   echo -e '✔ \e[32mNode version OK\e[0m'
+# fi
+
+# Get NPM version
+NPM_VERSION=$(npm -v | cut -b 1)
+if [ $NPM_VERSION != 6 ]; then
+  echo -e '\e[33mNPM version 6 or higher is higly recomended!\e[0m'
+  sleep 2
+else
+  echo -e '\e[32m✔ NPM version OK\e[0m'
+fi
+
+# Make sure ng (Angular CLI is installed globaly)
+NG=$(basename $(which ng))
+if [[ $NG != "ng" ]]; then
+  echo -e '\e[31m✘ Angular CLI not installed. Install with "npm install -g @angular/cli"\e[0m'
+  exit -1
+else
+  echo -e '\e[32m✔ Angular CLI Installed\e[0m'
+fi
+
+# Make sure we have enough memory available for npm install (ugh)
+MEMFREE=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+SWPFREE=$(cat /proc/meminfo | grep SwapFree | awk '{print $2}')
+if [ $(($MEMFREE + $SWPFREE)) -lt 1000000 ]; then
+  echo -e '\e[31m✘ Not enough memory available!\e[0m'
+  exit -1
+else
+  echo -e '\e[32m✔ Enough memory\e[0m'
+fi
+
+# Build the Angular App
+echo '- Building Angular App'
+cd app
+npm install
+ng build --prod
+cd ..
+
+# Create .env
+cp .env.sample .env
+
+# Generate random passwords and tokens
+for i in `seq 1 3`; do
+  R=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  sed -i -e "0,/__PLACEHOLDER__/{s/__PLACEHOLDER__/$R/}" .env2
+done
+
+# Start docker containers
+docker-compose up -d
+
+# Wait 5 seconds to make sure the containers are up and running
+sleep 5
+
+# Get the folder name, docker uses it as a prefix
+DIR=$(basename $PWD)
+
+# Execute setup scripts
+docker exec $(docker ps | grep "$DIR"_postgres_1 | awk '{print $11}') "/bin/bash" "/setup/install.sh"
+docker exec $(docker ps | grep "$DIR"_mongo_1 | awk '{print $11}') "/bin/bash" "/setup/install.sh"
+
+# Restart the containers
+docker-compose restart
+
+PORT=$(cat .env | grep NODE_PORT | cut -b 11-15)
+echo "Done. API running on port $PORT"
