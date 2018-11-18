6fdfaeeca37c61c46ef85df49628c173dc1603c3
olback <git@olback.net>
2018-11-18 23:08:17 +0100

    bcds component

---
+++ app/src/app/bcds/bcds.component.ts
@@ -0,0 +1,182 @@
+import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import * as Quagga from 'quagga';
+import { Bcds } from '../_classes/bcds';
+import { BcdsService } from '../_services/bcds.service';
+import { InfoBarService } from '../_services/info-bar.service';
+
+@Component({
+  selector: 'app-bcds',
+  templateUrl: './bcds.component.html',
+  styleUrls: ['./bcds.component.scss']
+})
+export class BcdsComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('addModal') addModal: ElementRef;
+
+  barcodeSamples: string[] = [];
+  barcode = '';
+  item = new Bcds('', '', '');
+  active = true;
+  requiredScans = 25;
+  err: string;
+
+  constructor(
+    private _bcdsService: BcdsService,
+    private _infobarService: InfoBarService
+  ) { }
+
+  ngAfterViewInit() {
+    Quagga.init(
+      {
+        numOfWorkers: navigator.hardwareConcurrency,
+        locate: true,
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          facingMode: 'environment',
+          target: document.querySelector('#scanner') // Or '#yourElement' (optional)
+        },
+        // photoSettings: {
+        //   fillLightMode: 'flash', /* or 'flash' */
+        //   focusMode: 'continuous'
+        // },
+        locator: {
+          patchSize: 'medium', // 'medium' | 'large'?
+          halfSample: true
+        },
+        decoder: {
+          readers: ['ean_reader', 'ean_8_reader', 'upc_reader'],
+          debug: {
+            showCanvas: true,
+            showPatches: true,
+            showFoundPatches: true,
+            showSkeleton: true,
+            showLabels: true,
+            showPatchLabels: true,
+            showRemainingPatchLabels: true,
+            boxFromPatches: {
+              showTransformed: true,
+              showTransformedBox: true,
+              showBB: true
+            }
+          }
+        }
+      },
+      err => {
+        if (err) {
+          console.log(err);
+          this.err = err;
+          return;
+        }
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+      }
+    );
+
+    Quagga.onDetected(data => {
+
+      if (data && this.active) {
+
+        this.barcodeSamples.push(data.codeResult.code);
+
+        if (this.barcodeSamples.length > this.requiredScans) {
+
+          this.active = false;
+          this.barcode = common(this.barcodeSamples);
+          this.barcodeSamples = [];
+          navigator.vibrate(75);
+
+          this._bcdsService.get(this.barcode).subscribe(
+            res => {
+              console.log('found an item');
+              this.item = res;
+              this.toggleModal();
+            },
+            err => {
+              this.active = true;
+              if (err.status === 404) {
+                this._infobarService.show(`Barcode ${this.barcode} not found`, 3000);
+              }
+            }
+          );
+
+        }
+
+        // console.log(data.codeResult.code);
+      }
+
+    });
+
+    Quagga.onProcessed(result => {
+      const drawingCtx = Quagga.canvas.ctx.overlay;
+      const drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute('width'), 10),
+            parseInt(drawingCanvas.getAttribute('height'), 10)
+          );
+          result.boxes
+            .filter(function(box) {
+              return box !== result.box;
+            })
+            .forEach(function(box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: 'green',
+                lineWidth: 2
+              });
+            });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: '#00F',
+            lineWidth: 2
+          });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: 'x', y: 'y' },
+            drawingCtx,
+            { color: 'red', lineWidth: 3 }
+          );
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    Quagga.stop();
+  }
+
+  toggleModal() {
+
+    this.active = !this.addModal.nativeElement.classList.toggle('show');
+
+  }
+
+  submitAdd(item: Bcds) {
+
+    console.log(item);
+    console.log('submitted');
+    this.toggleModal();
+
+  }
+
+  // submitEdit(item: Bcds) {
+  // }
+
+  // submiteDelete(barcode: string) {
+  // }
+
+}
+
+function common(arr: Array<string>) {
+  return arr.sort((a, b) => arr.filter(v => v === a).length - arr.filter(v => v === b).length).pop();
+}
+
