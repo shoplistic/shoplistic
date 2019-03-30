d77b6b17f38b67cb219bd08a0d8de931e6150494
Edwin Svensson <git@olback.net>
2019-03-30 01:33:13 +0100

    export mongodb to json file

--- scripts/mongo/dump.sh
+++ scripts/mongo/dump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mongodump --db $MONGO_INITDB_ROOT_USERNAME --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD -o /export/
+mongoexport --db $MONGO_INITDB_ROOT_USERNAME --username $MONGO_INITDB_ROOT_USERNAME --collection users --password $MONGO_INITDB_ROOT_PASSWORD -o /export/$MONGO_INITDB_ROOT_USERNAME/dump.json
