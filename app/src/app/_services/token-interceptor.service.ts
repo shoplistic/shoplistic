156e7f2dcd3e1b0277edcb10bc9c2d332b9bbe7c
Edwin Svensson <git@olback.net>
2019-09-01 20:15:04 +0200

    only intercept requests to our own api

--- app/src/app/_services/token-interceptor.service.ts
+++ app/src/app/_services/token-interceptor.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,21 @@ export class TokenInterceptorService implements HttpInterceptor {
 
   intercept(req, next) {
 
-    const tokenizedReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${this._authService.getToken()}`
-      }
-    });
+    if ((req.url as string).includes(environment.apiUrl)) {
 
-    return next.handle(tokenizedReq);
+      const tokenizedReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this._authService.getToken()}`
+        }
+      });
+
+      return next.handle(tokenizedReq);
+
+    } else {
+
+      next.handle(req.clone());
+
+    }
 
   }
 
