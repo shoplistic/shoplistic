2fc83fbe93999fb48baa61984ab7a94a5b53ce9c
Edwin Svensson <git@olback.net>
2019-09-29 01:40:58 +0200

    edit scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-read -p "Are you sure? " -n 1 -r
+read -p "Are you sure? (y/n): " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo 'About to delete "data" and "export". Waiting for 5 seconds...'
+  echo 'About to delete "data", "export" and "server/node_modules". Waiting for 5 seconds...'
   sleep 5
-  rm -rf 'data' 'export'
+  rm -rf 'data' 'export' 'server/node_modules'
   echo 'Done'
 fi
